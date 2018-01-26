4610f9e8c7ae8a3ffefc692551f8d802a66ad24b
Duc Trung <ducdt1992@gmail.com>
2018-01-26 08:11:49 +0700

    + refactor

---
+++ PurpleAirCalculator.js
@@ -0,0 +1,22 @@
+const signs = [
+    'Bạch Dương',
+    'Kim Ngưu',
+    'Song Tử',
+    'Cự Giải',
+    'Sư Tử',
+    'Xử Nữ',
+    'Thiên Bình',
+    'Bọ Cạp',
+    'Nhân Mã',
+    'Ma Kết',
+    'Bảo Bình',
+    'Song Ngư',
+];
+
+export default class PurpleAirCalculator {
+    static getSign(position) {
+        return signs[position];
+    }
+
+    static calculate() {}
+}
